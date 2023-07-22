7e2807024f6340ed3c033c8baca335424deb1180
Imtiyaz Ahmed <imtahmed9@gmail.com>
2023-07-22 19:33:32 +0000

    Code changes

--- visa_request_information/webapp/controller/RequestInformation.controller.js
+++ visa_request_information/webapp/controller/RequestInformation.controller.js
@@ -1,15 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("visarequestinformation.controller.RequestInformation", {
             onInit: function () {
-
+                var oData = {
+                    sapCustomer: "0",
+                    acmeCorporation: "0",
+                }
+                var oModel = new JSONModel(oData);
+                this.getView().setModel(oModel, "oModelControl");
             }
         });
     });
