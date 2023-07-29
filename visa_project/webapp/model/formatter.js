379f94c7d102e7c2d026176331f108e67d2cd8e6
Imtiyaz Ahmed <imtahmed9@gmail.com>
2023-07-29 23:04:08 +0000

    Updated code

--- visa_project/webapp/model/formatter.js
+++ visa_project/webapp/model/formatter.js
@@ -8,6 +8,21 @@ sap.ui.define([], function () {
 				case "COMPLETED":
 					return "Success";
 			}
+		}, fnSupplierChangeColor: function (sStatus) {
+			switch (sStatus) {
+				case "ONBOARDING ERROR":
+					return "Warning";
+				case "ACCEPTS CARD PAYMENTS":
+				case "ONBOARDED TO VBIP":
+					return "Success";
+				case "NOT ONBOARDED":
+					return "Error";
+			}
+		}, fnSapCustomerChangeColor: function (sStatus) {
+			switch (sStatus) {
+				case "YES":
+					return "Success";
+			}
 		}
 	};
 });
